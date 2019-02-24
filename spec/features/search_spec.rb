78a97db06ba494e38d9fb244faab0f7a694388c0
Harper Bellows <harper.bellows@gmail.com>
2019-02-24 15:55:30 -0700

    Add feature spec for city search to index view

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 describe 'Search feature' do
   context 'User enters a location in the search box' do
     it 'the show page displays statistics for that location' do
-      
+      visit '/'
+
+      fill_in :q,	with: "denver, co" 
+      click_on 'Search'
+
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content('Denver')
     end
   end
 end

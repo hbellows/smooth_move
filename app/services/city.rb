5d21860ed17394a0fdce20a6ec9126593cfc7760
Harper Bellows <harper.bellows@gmail.com>
2019-02-24 16:31:35 -0700

    Refactor out unused methods

--- app/services/city.rb
+++ app/services/city.rb
@@ -1,11 +1,9 @@
 class City
 
-  attr_reader :geo_id, :city_name, :city, :state
+  attr_reader :geo_id, :city_name
+
   def initialize(data)
-    split_city_name = data[4].split
     @geo_id = data[0]
     @city_name = data[4]
-    @city = split_city_name[0]
-    @state = split_city_name[1]
   end
 end

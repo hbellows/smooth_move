3777157fec01d48e27beb8676af3abc00270ba6f
Harper Bellows <harper.bellows@gmail.com>
2019-02-24 16:02:54 -0700

    Add small refactors for easier viewing

--- app/services/city.rb
+++ app/services/city.rb
@@ -1,9 +1,11 @@
 class City
 
+  attr_reader :geo_id, :city_name, :city, :state
   def initialize(data)
-    city_name = data[4].split
+    split_city_name = data[4].split
     @geo_id = data[0]
-    @city = city_name[0]
-    @state = city_name[1]
+    @city_name = data[4]
+    @city = split_city_name[0]
+    @state = split_city_name[1]
   end
 end

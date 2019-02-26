b21f9c8a5023413dd69c14cfb4f1f2e77ac22d24
Harper Bellows <harper.bellows@gmail.com>
2019-02-25 21:57:15 -0700

    Add location and favorite tables. Add passing relationship tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_003448) do
+ActiveRecord::Schema.define(version: 2019_02_26_043745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.boolean "active", default: true
+    t.bigint "user_id"
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_favorites_on_location_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "geo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +40,6 @@ ActiveRecord::Schema.define(version: 2019_02_24_003448) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "locations"
+  add_foreign_key "favorites", "users"
 end
